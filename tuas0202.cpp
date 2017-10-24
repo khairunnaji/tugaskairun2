f7137ba19f6e16a5f34718fe26703a3d772584a0
khairunnaji <32257830+khairunnaji@users.noreply.github.com>
2017-10-24 14:48:21 +0800

    “Output bermacam gaya”

--- tuas0202.cpp
+++ tuas0202.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-	int NIlai=0;
+int main () {
+	
+	int nilai=0;
 	int Nilai=1;
-	int NIlaI=2;
-	int NIlAi=3;
+	int NILAI=2;
+	int niLAi=3;
+	
 	double angka(10.5),Angka,ANGKA(3.14);
 	int a(10),b(7),c(12);
+	
+	cout << nilai<<" : "<<Nilai<<" | "<<NILAI<<" "<<niLAi<<endl;
+	cout << angka <<"\n";
+	cout << Angka <<endl<<ANGKA;
 }
